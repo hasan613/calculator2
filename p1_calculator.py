73056bc2f2c1f9b085a9db1f01ed78a069a98384
hasan613 <gasanpasha2003@gmail.com>
2026-02-14 22:01:41 +0300

    refactor: apply linting fixes (closes #61)

--- p1_calculator.py
+++ p1_calculator.py
@@ -1,20 +1,30 @@
-def calculator():
-    no_1 = int(input("enter your first number"))
-    operation = int(input("enter  arithmetic operation  like +,-,*,/"))
-    no_2 = int(input("enter your  second number"))
+"""Модуль для выполнения простых арифметических операций."""
 
-    if operation =='+':
-        print(f"the sum of {no_1} + {no_2} are {no_1 + no_2}")
-    elif  operation =='-':
-        print(f"the diff of {no_1}  - {no_2} are {no_1 - no_2}")
-    elif operation =='*':
-        print(f"the product of {no_1} * {no_2} are {no_1 * no_2}")
-    elif  operation =='/':
-        if no_2 == 0:
-            print("it gives infinity as it is not divided by zero")
+
+def calculator() -> None:
+    """Функция запрашивает ввод данных у пользователя и выводит результат
+    операции."""
+    try:
+        no_1 = int(input("enter your first number: "))
+        operation = input("enter arithmetic operation like +,-,*,/: ")
+        no_2 = int(input("enter your second number: "))
+
+        if operation == "+":
+            print(f"the sum of {no_1} + {no_2} is {no_1 + no_2}")
+        elif operation == "-":
+            print(f"the diff of {no_1} - {no_2} is {no_1 - no_2}")
+        elif operation == "*":
+            print(f"the product of {no_1} * {no_2} is {no_1 * no_2}")
+        elif operation == "/":
+            if no_2 == 0:
+                print("it gives infinity as it is not divided by zero")
+            else:
+                print(f"the quotient of {no_1} / {no_2} is {no_1 / no_2}")
         else:
-            print(f"the sum of {no_1}  / {no_2} are {no_1 / no_2}")
-    else:
-        print("invalid output")
+            print("invalid operation")
+    except ValueError:
+        print("Invalid input! Please enter numbers only.")
 
-calculator()
+
+if __name__ == "__main__":
+    calculator()
